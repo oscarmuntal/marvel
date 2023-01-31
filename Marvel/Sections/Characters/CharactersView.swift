8455a1f946a5f2583d73ed505552b0c198302ec2
Oscar <oscar.muntal@gmail.com>
2023-01-31 21:45:42 +0100

    [characters]: - removed default ViewController.swift - added CharactersView.swift with dummy background

--- Marvel/Sections/Characters/CharactersView.swift
+++ Marvel/Sections/Characters/CharactersView.swift
@@ -1,19 +1,18 @@
 //
-//  ViewController.swift
+//  CharactersView.swift
 //  Marvel
 //
 //  Created by A on 31/1/23.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class CharactersView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        
     }
-
-
 }
-
