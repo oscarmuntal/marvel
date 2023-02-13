3c5f9fbcc9d1076dc3a83a26accb0716065a6fc1
Òscar Muntal <oscar.muntal@gmail.com>
2023-02-13 12:37:56 +0100

    [Testing]: adding tests to CharactersBuilderTests

--- MarvelTests/CharactersBuilderTests.swift
+++ MarvelTests/CharactersBuilderTests.swift
@@ -55,4 +55,45 @@ class CharactersBuilderTests: XCTestCase {
         XCTAssertTrue(presenter.interactor is CharactersInteractor, "Expected presenter's interactor to be of type CharactersInteractor")
         XCTAssertTrue(presenter.router is CharactersRouter, "Expected presenter's router to be of type CharactersRouter")
     }
+    
+    func testBuildPresenter() {
+        // given
+        let presenter = charactersBuilder.buildPresenter()
+        
+        // then
+        XCTAssertNotNil(presenter)
+        XCTAssertTrue(presenter is CharactersPresenterContract)
+        
+    }
+    
+    func testBuildPresenter2() {
+        // given
+        let presenter = charactersBuilder.buildPresenter()
+        
+        // when
+        let charactersPresenter = presenter as! CharactersPresenter
+        
+        // then
+        XCTAssertNotNil(charactersPresenter.wireframe)
+        XCTAssertNotNil(charactersPresenter.interactor)
+        XCTAssertNotNil(charactersPresenter.router)
+    }
+    
+    func testBuildInteractor() {
+        // given
+        let interactor = charactersBuilder.buildInteractor()
+        
+        // then
+        XCTAssertNotNil(interactor)
+        XCTAssertTrue(interactor is CharactersInteractorContract)
+    }
+    
+    func testBuildRouter() {
+        // given
+        let router = charactersBuilder.buildRouter()
+        
+        // then
+        XCTAssertNotNil(router)
+        XCTAssertTrue(router is CharactersRouterContract)
+    }
 }
